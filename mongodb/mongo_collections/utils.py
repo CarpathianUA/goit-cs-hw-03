47f42e5e2aaaeec6b482f0270ff2aa9d673f011e
Roman Slipchenko <roman.slipchenko@restream.io>
2024-03-09 23:27:50 +0200

    feat(utils): add exception handler wrapper

--- mongodb/mongo_collections/utils.py
+++ mongodb/mongo_collections/utils.py
@@ -1,3 +1,25 @@
+def exception_handler(func):
+    """
+    A decorator that wraps the passed in function and catches exceptions.
+
+    Args:
+        func: The function to wrap.
+
+    Returns:
+        The wrapper function.
+    """
+
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            print(f"An error occurred in {func.__name__}: {e}")
+            return None
+
+    return wrapper
+
+
+@exception_handler
 def find_all_records(collection):
     """
     Find all records in the given collection.
@@ -9,6 +31,7 @@ def find_all_records(collection):
         print(record)
 
 
+@exception_handler
 def find_record_by_name(collection, name):
     """
     Find a record in the collection by name.
@@ -24,6 +47,7 @@ def find_record_by_name(collection, name):
         print(f"Record {name} not found")
 
 
+@exception_handler
 def add_new_record(collection, name, author, price, isbn, tags):
     """
     Function to add a new record to a collection.
@@ -36,22 +60,20 @@ def add_new_record(collection, name, author, price, isbn, tags):
     - isbn: the ISBN of the record
     - tags: the tags associated with the record
     """
-    try:
-        result = collection.insert_one(
-            {
-                "name": name,
-                "author": author,
-                "price": price,
-                "isbn": isbn,
-                "tags": tags,
-            }
-        )
-        print(f"Added {name} to collection {collection.name}")
-        print("ID of the inserted document:", result.inserted_id)
-    except Exception as e:
-        print(e)
+    result = collection.insert_one(
+        {
+            "name": name,
+            "author": author,
+            "price": price,
+            "isbn": isbn,
+            "tags": tags,
+        }
+    )
+    print(f"Added {name} to collection {collection.name}")
+    print("ID of the inserted document:", result.inserted_id)
 
 
+@exception_handler
 def update_record_price(collection, name, new_price):
     """
     Update the price of a record in the collection.
@@ -64,17 +86,14 @@ def update_record_price(collection, name, new_price):
     Returns:
         The updated record.
     """
-    try:
-        if collection.count_documents({"name": name}) > 0:
-            collection.update_one({"name": name}, {"$set": {"price": new_price}})
-            print(f"Updated price of {name} to {new_price}")
-        else:
-            print(f"Record {name} not found")
-        return find_record_by_name(collection, name)
-    except Exception as e:
-        print(e)
+    if collection.count_documents({"name": name}) > 0:
+        collection.update_many({"name": name}, {"$set": {"price": new_price}})
+        print(f"Updated price of {name} to {new_price}")
+    else:
+        print(f"Record {name} not found")
 
 
+@exception_handler
 def add_new_tag_to_record(collection, name, tag):
     """
     Add a new tag to a record in the collection.
@@ -87,18 +106,16 @@ def add_new_tag_to_record(collection, name, tag):
     Returns:
         The updated record with the new tag added.
     """
-    try:
-        if collection.count_documents({"name": name}) > 0:
-            collection.update_one({"name": name}, {"$addToSet": {"tags": tag}})
-            print(f"Added {tag} to {name}")
-            return find_record_by_name(collection, name)
-        else:
-            print(f"Record {name} not found")
-            return None
-    except Exception as e:
-        print(e)
+    if collection.count_documents({"name": name}) > 0:
+        collection.update_many({"name": name}, {"$addToSet": {"tags": tag}})
+        print(f"Added {tag} to {name}")
+        return find_record_by_name(collection, name)
+    else:
+        print(f"Record {name} not found")
+        return None
 
 
+@exception_handler
 def delete_record_by_name(collection, name):
     """
     Deletes a record from the collection by name.
@@ -107,22 +124,17 @@ def delete_record_by_name(collection, name):
         collection: The collection from which to delete the record.
         name: The name of the record to be deleted.
     """
-    try:
-        if collection.count_documents({"name": name}) > 0:
-            collection.delete_one({"name": name})
-            print(f"Deleted {name} from collection {collection.name}")
-        else:
-            print(f"Record {name} not found")
-    except Exception as e:
-        print(e)
+    if collection.count_documents({"name": name}) > 0:
+        collection.delete_many({"name": name})
+        print(f"Deleted {name} from collection {collection.name}")
+    else:
+        print(f"Record {name} not found")
 
 
+@exception_handler
 def delete_all_records(collection):
     """
     Deletes all records from the given collection.
     """
-    try:
-        collection.delete_many({})
-        print("Deleted all records from collection...Done!")
-    except Exception as e:
-        print(e)
+    collection.delete_many({})
+    print("Deleted all records from collection...Done!")
